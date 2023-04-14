117ade2aec9b348035979d997f1934d362769316
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-04-14 16:13:20 +0100

    refactor:  Move file (AppRouter.tsx) to the "routes" folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './styles/index.css';
-import { AppRoutes } from './AppRoutes';
+import { AppRoutes } from './routes/AppRoutes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
