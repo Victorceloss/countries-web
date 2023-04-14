29a3785fe259e3aa6b2f5d313ca709b5dedec11c
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-04-14 16:05:18 +0100

    feat: definition of the main page that will be rendered with react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import './styles/index.css';
 import Home from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
