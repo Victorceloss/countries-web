40722caca7cbed5297e0c6fc11a827fc39945b49
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-04-14 16:10:36 +0100

    refactor: remove the react-router-dom logic from the "main.tsx" file and move it to "AppRoutes.tsx".

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import './styles/index.css';
-import Home from './pages/Home';
+import { AppRoutes } from './AppRoutes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
-    </BrowserRouter>
+    <AppRoutes />
   </React.StrictMode>
 )
