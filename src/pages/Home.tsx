e51901eea6720c730f8573fd29fff796ad0c5399
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-04-14 17:04:46 +0100

    feat: Add typing to the "Home" function of the Home.tsx file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,18 @@ import React from "react";
 
 import { Header } from "../components/Header/Header";
 
-const Home = () => {
+const Home: React.FC = (): JSX.Element => {
   return (
     <>
     <Header />
+    <main className="main">
+      <section className="main-section-search">
+        <input type="text" name="name-country" className="input-name" placeholder="Ex: United States..." />.
+      </section>
+      <section className="main-section-all">
+
+      </section>
+    </main>
     </>
   );
 };
