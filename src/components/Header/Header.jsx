1fa64a135ded691d47f493a385d8913258d7e7a3
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-04-14 15:47:47 +0100

    feat: add new elements to the Header component
    Added main elements, such as: Header Title, Button for Dark-Mode and for the Repository on Github

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,15 @@ import React from "react";
 export const Header = () => {
   return (
     <header className="header">
-
+      <h1 className="header-title">Where in the world?</h1>
+      <nav className="header-nav">
+        <button className="nav-button" id="toggle-darkmode">
+          Dark Mode
+        </button>
+        <button className="nav-button" id="github-button">
+          Github
+        </button>
+      </nav>
     </header>
   );
 };
