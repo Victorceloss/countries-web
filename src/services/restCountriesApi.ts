013cf5f638e2caeff749eb894e7e8ac28a0eafd6
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-04-15 12:44:29 +0100

    chore: Remove a "Country Interface" do the restCountriesApi.ts

--- src/services/restCountriesApi.ts
+++ src/services/restCountriesApi.ts
@@ -1,13 +1,7 @@
 import axios from "axios";
 import axiosInstance from "../config/axiosConfig";
 
-export interface Country {
-  name: string,
-  capital: string,
-  population: string
-}
-
-export const getAllCountriesData = async (): Promise<Country[]> => {
+export const getAllCountriesData = async () => {
   const response = await axiosInstance.get('/all');
   const data = response.data;
 
