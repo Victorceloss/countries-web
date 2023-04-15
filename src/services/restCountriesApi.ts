e5a9f589593fb7f399c57e2d28b775a8d19b838b
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-04-15 12:29:16 +0100

    chore: Changed the axios import and created the "Country" interface for Typescript.

--- src/services/restCountriesApi.ts
+++ src/services/restCountriesApi.ts
@@ -1,8 +1,15 @@
 import axios from "axios";
+import axiosInstance from "../config/axiosConfig";
 
-export const getAllCountriesData = async () => {
-  const response = await axios.get('/all');
+export interface Country {
+  name: string,
+  capital: string,
+  population: string
+}
+
+export const getAllCountriesData = async (): Promise<Country[]> => {
+  const response = await axiosInstance.get('/all');
   const data = response.data;
 
-  return await data.json();
+  return await data;
 }
