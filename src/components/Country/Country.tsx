618cc96567ec01ff4c256041e8c3f3d352ad47c3
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-04-15 12:44:51 +0100

    feat: Creating the "Country.tsx" component

---
+++ src/components/Country/Country.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type CountryProps = {
+  countryData: any;
+}
+
+export const Country: React.FC<CountryProps> = ({countryData}) => {
+  return (
+    <article className="all-article">
+      
+    </article>
+  );
+};
