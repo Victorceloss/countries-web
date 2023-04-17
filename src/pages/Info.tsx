787803cdaf17fb4ee0b3ced104fb5932ffa48bf5
Victor Vasconcelos <128932311+Victorceloss@users.noreply.github.com>
2023-04-17 10:22:21 +0100

    feat: Creating and Importing the "Info.tsx" Component

---
+++ src/pages/Info.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Header } from "../components/Header/Header";
+
+const Info: React.FC = (): JSX.Element => {
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default Info;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Home from "../pages/Home";
+import Info from "../pages/Info";
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/info/:name" element={<Info />} />
       </Routes>
     </BrowserRouter>
   )
